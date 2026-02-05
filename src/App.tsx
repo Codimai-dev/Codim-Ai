1a654c9db4ab38e9eb32fe62ce2494d154b8a193
Nagaraj N P <nagarajnp@2004gmail.com>
2026-02-05 14:12:57 +0530

    Rotering fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import OurTech from './pages/OurTech';
 import Article from './pages/Article';
